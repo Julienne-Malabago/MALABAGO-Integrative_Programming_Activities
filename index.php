f65606dc21a770e41e5730475a3001a86b91f5b2
Julienne-Malabago <julienne.h.malabago@gmail.com>
2025-02-15 23:10:01 +0800

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $weather_conditions = array("rain", "sunshine", "clouds", "hail", "sleet", "snow", "wind");
-            
-    echo "<p>We've seen all kinds of weather this month. At the beginning of the month, we had " .
-        "$weather_conditions[5] and $weather_conditions[6]. Then came $weather_conditions[1] with a few " .
-        "$weather_conditions[2] and some $weather_conditions[0]. At least we didn't get any " .
-        "$weather_conditions[3] or $weather_conditions[4].</p>";
-?>
